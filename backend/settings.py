df7f9c7b290f1f77f20f3d287b2d6a379ae6396f
josh richards <joshuarichards001@gmail.com>
2020-09-04 22:25:06 +1200

    Minor adjustments

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .secretkey import secretkey
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,5 +136,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = [
-    'https://financial-freedom.netlify.app/'
+    'https://financial-freedom.netlify.app'
 ]
